a33e25d9b848c561ec155c42a13dcaf71e747b32
1nurs123 <161112496+1nurs123@users.noreply.github.com>
2025-08-08 14:55:34 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,14 @@
+import './globals.css';
+
 export const metadata = { title: 'Sales Reports' };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body style={{ margin: 0, fontFamily: 'Arial, system-ui' }}>
-        {children}
+      <body>
+        <div className="container">
+          {children}
+        </div>
       </body>
     </html>
   );
